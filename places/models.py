b15a9ca17512ce87e7674a8dc37c1c39cb5ef8f7
zhanyll <janylayka95@gmail.com>
2021-07-19 18:44:05 +0600

    added user foreign key

--- places/models.py
+++ places/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Place(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    decription = models.TextField()
+    decription = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     view_count = models.IntegerField(default=0)
